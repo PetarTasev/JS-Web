6e04d7829b85548836000ffa3be37dfe0b5e91d8
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-10-26 17:25:11 +0300

    on prend une petite pause

--- JsBackEnd/JS-Web/noSQLdemo/hbs_demo/index.js
+++ JsBackEnd/JS-Web/noSQLdemo/hbs_demo/index.js
@@ -1,15 +1,21 @@
 const express = require('express')
+const { startSession } = require('mongoose')
 const hbs = require('express-handlebars').create({
     extname: '.hbs'
 })
+const mongoose = require('mongoose')
+const connString = 'mongodb://localhost:27017/testdb0.1'
 
+async function start() {
 const app = express()
 app.engine('.hbs', hbs.engine)
 app.set('view engine', '.hbs')
 
 app.get('/', (req, res) => {
-
+    res.render('home')
 })
 
+mongoose.mongo.connect(connString)
 
-app.listen(3000)
+app.listen(3000, () => console.log('Sever listening on port 3000'))
+}
