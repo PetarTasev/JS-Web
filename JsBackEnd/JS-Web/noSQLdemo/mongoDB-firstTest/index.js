7754865a03afc250995bb963611c3bc1465ce67f
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-11-09 00:35:43 +0200

    commit

--- JsBackEnd/JS-Web/noSQLdemo/mongoDB-firstTest/index.js
+++ JsBackEnd/JS-Web/noSQLdemo/mongoDB-firstTest/index.js
@@ -2,15 +2,7 @@ const mongodb = require('mongodb')
 
 const connectingString = 'mongodb://127.0.0.1:27017/'
 
-const connection = new mongodb.MongoClient(connectingString, {
-    useUnifiedTopology: true
-})
-
-connection.connect((err, client) => {
-    console.log("database connected")
-    
-})
-
+start()
 
 
 async function start() {
