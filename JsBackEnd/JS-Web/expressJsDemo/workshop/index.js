0a8e57d1f9631f82dfd44cd838974b50049d7f96
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-11-14 22:43:32 +0200

    test

--- JsBackEnd/JS-Web/expressJsDemo/workshop/index.js
+++ JsBackEnd/JS-Web/expressJsDemo/workshop/index.js
@@ -3,6 +3,9 @@ const hbs = require('express-handlebars').create({
     extname: '.hbs'
 })
 
+const homeController = require('./controlers/homeControler')
+const defaultController = require('./controlers/defaultController')
+
 const app = express()
 
 app.engine('.hbs', hbs.engine)
@@ -12,4 +15,11 @@ app.set('view engine', '.hbs')
 app.use(express.urlencoded({ extended: true}))
 app.use('/static', express.static('static'))
 
+app.use(homeController)
+
+
+
+app.all('*', defaultController)
+
 app.listen(3000, () => console.log('Server listening on port 3000'))
+
