e33324c9cff2244a4b802f2fd1b029a2c8ee89dc
Petar Stefanov <petar.g.stefanov@gmail.com>
2022-11-15 23:48:25 +0200

    implimented catalog and details

--- JsBackEnd/JS-Web/expressJsDemo/workshop/controlers/catalogController.js
+++ JsBackEnd/JS-Web/expressJsDemo/workshop/controlers/catalogController.js
@@ -1,15 +1,28 @@
+const { getAll, getById } = require('../services/accomodationServices')
+
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
+    const rooms = getAll()
     res.render('catalog', {
-        title: 'All Accomodation'
+        title: 'All Accomodation',
+        rooms
     })
 })
 
 router.get('/:id', (req, res) => {
-    res.render('details', {
-        title : 'Accomodation details'
-    })
+    const roomId = req.params.id;
+    const room = getById(roomId)
+
+    if (room) {
+        res.render('details', {
+            title: 'Accomodation details',
+            room
+        })
+    } else {
+        res.render('404')
+    }
+
 })
 
 module.exports = router
